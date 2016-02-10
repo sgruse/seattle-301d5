de94af05b623910b8b9b9a21e05a2fde91fbef71
Sam Gruse <sgruse89@gmail.com>
2016-02-09 16:13:26 -0800

    my version of class-07

--- class-07-functional-programming/pair-assignment/starter-code/scripts/article.js
+++ class-07-functional-programming/pair-assignment/starter-code/scripts/article.js
@@ -1,5 +1,16 @@
 // TODO: Wrap the entire contents of this file in an IIFE.
 // Pass in to the IIFE a module, upon which objects can be attached for later access.
+// at the end of this we can use module to reference everything on the inside.
+//  var stuff = 'stuff';
+//  module.stuff = stuff;
+// When i export this to the window, I am attaching this object to the window so that we can reference it, >>>Expose Article to the window so we can access Article and its properties from the window. Window.Article = Article which means we can access everything in Article from the window.  Do this all at the end.
+(function(module) {
+
+  module.Article = Article;
+
+  put shit in here.
+}(window));
+
 function Article (opts) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
@@ -36,6 +47,8 @@ Article.loadAll = function(rawData) {
   });
 };
 
+// are article.all array will now contain all of the objects with the function
+
 // This function will retrieve the data from either a local or remote source,
 // and process it, then hand off control to the View.
 // TODO: Refactor this function, so it accepts an argument of a callback function (likely a view function)
@@ -54,12 +67,17 @@ Article.fetchAll = function() {
 };
 
 // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+// we are taking an array of objects, mapping to each item in that array, (accepting article as parameter which is each article in the array) targeting the body of each object and counting the amount word in each body.
 Article.numWordsAll = function() {
   return Article.all.map(function(article) {
-    return // Get the total number of words in this article
+    return article.body.match(/\b\w+/g).length;
+    // return // Get the total number of words in this article
   })
+  // a, b represent previous and current to squash together and calculate the total sum of the arry.
+  // we use returns so we don't have to declare variables to hold the information.
   .reduce(function(a, b) {
-    return // Sum up all the values in the collection
+    return a + b;
+    // Sum up all the values in the collection
   })
 };
 
